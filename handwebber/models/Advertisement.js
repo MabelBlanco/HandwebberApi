996f0487035d14e196f0bad130e139006347613c
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2023-02-12 14:07:17 +0100

    Advertisement model created 	nuevo archivo:  handwebber/models/Advertisement.js 	modificado:     handwebber/models/index.js

---
+++ handwebber/models/Advertisement.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+//Esquema
+const adSchema = mongoose.Schema({
+  active: { type: Boolean, required: true, default: true },
+  name: { type: String, required: true },
+  image: { type: String },
+  description: { type: String, required: true },
+  custom: { type: Boolean, required: true, default: false },
+  price: { type: Number, required: true },
+  stock: { type: Number, default: 0 },
+  tag: { type: String, required: true },
+  subscriptions: [String],
+  creation: { type: Date, default: Date.now },
+  update: { type: Date, default: Date.now },
+});
+
+//Crear modelo
+const Advertisement = mongoose.model('Advertisement', adSchema);
+
+//Exportar modelo
+module.exports = Advertisement;

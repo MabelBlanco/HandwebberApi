79ab7ad49e66c78c74c9e8f68ddd8bd1bc4f7e12
Josep Valls <alfredvays@gmail.com>
2023-02-18 23:11:47 +0100

    app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
-const errorResponseConfigure = require('./lib/errorResponseConfigure');
+const errorResponser = require('./lib/errorResponseConfigure');
 
 var app = express();
 

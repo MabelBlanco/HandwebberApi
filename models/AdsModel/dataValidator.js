69e446f849332e0f9a0c76033c4677aae3949317
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2023-02-20 14:13:02 +0100

    Auth to create ads working

--- models/AdsModel/dataValidator.js
+++ models/AdsModel/dataValidator.js
@@ -92,7 +92,6 @@ module.exports = function adDataValidator(method) {
         .withMessage(
           `tags must be an array of strings containing one tag at least`
         ),
-      //body('idUser').exists().isString().withMessage('idUser not provided'),
     ];
   }
 };
